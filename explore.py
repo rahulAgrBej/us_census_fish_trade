9b83712e453ab698e7bf088ab362bfad070671cc
rahulAgrBej <rahul.agrawal.b@gmail.com>
2021-01-12 14:58:29 -0500

    turns us census results into csv files

--- explore.py
+++ explore.py
@@ -1,6 +1,18 @@
 import requests
 import json
+import pprint
 
+
+def makeCSV(data):
+    csvStr = ''
+
+    for row in data:
+        for cell in row:
+            csvStr += str(cell)
+            csvStr += ','
+        csvStr += '\n'
+    
+    return csvStr
 
 EXPORT_URL = 'https://api.census.gov/data/timeseries/intltrade/exports/hs'
 IMPORT_URL = 'https://api.census.gov/data/timeseries/intltrade/imports/hs'
@@ -22,9 +34,14 @@ payload = {}
 payload['key'] = API_KEY
 payload['get'] = 'I_COMMODITY,GEN_VAL_MO'
 payload['time'] = '2013-01'
-payload['COMM_LVL'] = 'HS2'
+payload['COMM_LVL'] = 'HS6'
+payload['AIR_WGT_MO'] = ''
 
 
 resp = requests.get(IMPORT_URL, params=payload)
-print(resp.status_code)
-print(resp.content)
+
+
+#pp = pprint.PrettyPrinter(indent=3)
+#print(resp.status_code)
+print(makeCSV(resp.json()))
+#pp.pprint(resp.json())

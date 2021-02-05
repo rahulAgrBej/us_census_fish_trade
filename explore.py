e72026fd2008ef42389db5eadc3312b19133fc6c
rahulAgrBej <rahul.agrawal.b@gmail.com>
2021-02-04 20:21:16 -0500

    works with multiple years requested

--- explore.py
+++ explore.py
@@ -27,7 +27,7 @@ https://api.census.gov/data/timeseries/intltrade/imports/hs?get=I_COMMO
 DITY,GEN_VAL_MO&time=2013-01&COMM_LVL=HS2
 """
 
-testCountryURL = 'https://api.census.gov/data/timeseries/intltrade/exports/hs?get=E_COMMODITY,CTY_CODE,CTY_NAME,ALL_VAL_MO,SUMMARY_LVL&YEAR=2017&COMM_LVL=HS2&E_COMMODITY=03&CTY_CODE=1220'
+testCountryURL = 'https://api.census.gov/data/timeseries/intltrade/exports/hs?get=E_COMMODITY,CTY_CODE,CTY_NAME,ALL_VAL_MO,SUMMARY_LVL&YEAR=2017&YEAR=2018&COMM_LVL=HS2&E_COMMODITY=03&CTY_CODE=1220'
 
 # make sure there are no extra characters in key
 API_KEY = API_KEY.rstrip('\n')
@@ -46,7 +46,7 @@ print('after request')
 
 pp = pprint.PrettyPrinter(indent=3)
 print(resp.status_code)
-if (resp.status_code >= 200) and (resp.status_code < 300):
+if resp.status_code == 200:
     incomingData = resp.json()
     print(makeCSV(incomingData))
 else:

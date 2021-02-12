d0f9deef52dc432e1e6f84497c403641507d9125
rahulAgrBej <rahul.agrawal.b@gmail.com>
2021-02-11 22:32:37 -0500

    MVP visualizations set for imports into US

--- analysis/imports.R
+++ analysis/imports.R
@@ -20,6 +20,10 @@ dataFormat.aggTransport <- function(fp) {
       str_detect(CTY_NAME, 'TOTAL FOR ALL COUNTRIES')
     )
   
+  # turning year and month into nums
+  imports$MONTH <- as.numeric(imports$MONTH)
+  imports$YEAR <- as.numeric(imports$YEAR)
+  
   # Get separate tables for each type of transport
   
   # Totals table
@@ -28,15 +32,15 @@ dataFormat.aggTransport <- function(fp) {
   
   # Gross Weight in KGs of shipments made by AIR
   airWGT <- subset(imports, select=c(AIR_WGT_MO, MONTH, YEAR))
-  airWGT <- dataFormat.addTypeCol(airWGT, 'AIR Shipping Weight')
+  airWGT <- dataFormat.addTypeCol(airWGT, 'AIR WGT')
   
   # Gross Weight in KGs of shipments made by Water
   waterWGT <- subset(imports, select=c(VES_WGT_MO, MONTH, YEAR))
-  waterWGT <- dataFormat.addTypeCol(waterWGT, 'WATER Shipping Weight')
+  waterWGT <- dataFormat.addTypeCol(waterWGT, 'WATER WGT')
   
   # Gross Weight in KGs of shipments made by van
   cntWGT <- subset(imports, select=c(CNT_WGT_MO, MONTH, YEAR))
-  cntWGT <- dataFormat.addTypeCol(cntWGT, 'CONTAINER Shipping Weight')
+  cntWGT <- dataFormat.addTypeCol(cntWGT, 'CONTAINER WGT')
   
   aggregateTable <- rbind(totals, airWGT, waterWGT, cntWGT)
   return(aggregateTable)
@@ -59,4 +63,10 @@ dataFormat.aggYears <- function(fpStart, startYr) {
 }
 
 tradeTransport <- dataFormat.aggYears('../tradeDataRecords/imports/importCountries', 2013)
+
+p <- ggplot() +
+  geom_line(data=tradeTransport, aes(MONTH, COUNTS)) +
+  scale_x_continuous(breaks=seq(1, 96, by=1)) +
+  facet_grid(TYPE~YEAR, scales='free_y')
+plot(p)
   
